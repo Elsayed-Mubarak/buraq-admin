6450c2dc366b033b1ba95f4460c66aa0766b94db
ammar <ammaralaa572@gmail.com>
2025-02-12 12:20:54 +0200

    fixing

--- src/app/dashboard/templates/template-manager/TemplateTable.tsx
+++ src/app/dashboard/templates/template-manager/TemplateTable.tsx
@@ -4,14 +4,14 @@ import React from "react";
 import TableManger from "@/components/common/TemplateManager";
 import { FormData } from "@/app/types/TemplateTypes";
 import { TemplateTableProps } from "@/app/types/templateManager-types/TemplateManagerTypes";
-import DraggableTable from "@/components/common/DragableTable/DragableTable";
+//import DraggableTable from "@/components/common/DragableTable/DragableTable";
 
 
 const TemplateTable: React.FC<TemplateTableProps> = ({ columns, data }) => {
   return (
     <div className="mt-6 bg-white shadow rounded-lg">
-      {/*<TableManger<FormData> columns={columns} data={data} />*/}
-      <DraggableTable />
+      <TableManger<FormData> columns={columns} data={data} />
+      {/*<DraggableTable />*/}
     </div>
   );
 };
